a19fb57d09ebdc67161e68e04eb943dbec476988
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-01-05 23:03:13 -0700

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "gatsby-link";
+
+export default () =>
+<div style={{ color:'tomato' }}>
+    <h1>Welcome to Gatsby</h1>
+    <p>What a world!</p>
+    <img src="https://i.chzbgr.com/full/4994391296/hB3E19313/" alt=""/>
+    <br/>
+    <div>
+        <Link to="/page-2/">Link</Link>
+    </div>
+</div>
---
+++ src/pages/page-2.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "gatsby-link";
+
+export default () => (
+    <div>
+        <h1>Hello World from my second Gatsby Page</h1>
+        <Link to="/">Back Home</Link>
+
+    </div>
+
+);
