f0866128aff894e4c3f4ec43e0daec44ef6a462c
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-01-06 21:46:04 -0700

    Counter

---
+++ src/pages/counter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Counter extends React.Component {
+    constructor(){
+        super()
+        this.state = { count:0 }
+    }
+    render() {
+        return(
+        <div>
+            <h2>Counter: </h2>
+            <p>current count: {this.state.count}</p>
+            <button onClick={() => this.setState({ count: this.state.count + 1})}>+</button>
+            <button>-</button>
+        </div>
+        )
+    }
+}
+
+export default Counter
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,9 @@ export default () =>
     <img src="https://i.chzbgr.com/full/4994391296/hB3E19313/" alt=""/>
     <br/>
     <div>
-        <Link to="/page-2/">Link</Link>
+        <Link to="/page-2/">Page 2</Link>
+    </div>
+    <div>
+        <Link to="/counter/">Counter</Link>
     </div>
 </div>
